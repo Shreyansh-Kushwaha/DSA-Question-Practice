b567edc3bdd6c577fe99a558d22eb93b12368c6e
Shreyansh Kushwaha <shreyanshkushwaha02@gmail.com>
2026-02-15 16:16:21 +0530

    Implement BinaryTree class and depthOfTree method

---
+++ Depth_Of_Tree.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+
+class Node{
+    int data;
+    Node left,right;
+    
+    public Node( int item ) {
+        data = item ;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+    
+    public BinaryTree() {
+        root = null;
+    }
+}
+
+
+public class Depth_Of_Tree
+{
+    
+
+	public static void main(String[] args) {
+		
+		BinaryTree bochu = new BinaryTree();
+		bochu.root = new Node(10);
+
+        bochu.root.left = new Node(5);
+        bochu.root.right = new Node(15);
+    
+        bochu.root.left.left = new Node(2);
+        bochu.root.left.right = new Node(7);
+    
+        bochu.root.right.left = new Node(12);
+    
+        System.out.println("Before The Inversion");
+		prettyPrint(bochu.root, "", false);
+		
+		invertTree(bochu.root);
+		
+		System.out.println("After the Inverison");
+		prettyPrint(bochu.root, "", false);
+	}
+    
+    
+    public static Node depthOfTree(Node root) {
+            
+            Stack<Integer> stuck = new Stack<>();
+            
+    }
+    
+	
+	public static void prettyPrint(Node root, String prefix, boolean isLeft) {
+        if (root == null) {
+            return;
+        }
+    
+        System.out.println(prefix + (isLeft ? "├── " : "└── ") + root.data);
+    
+        // Enter the next generation
+        prettyPrint(root.left, prefix + (isLeft ? "│   " : "    "), true);
+        prettyPrint(root.right, prefix + (isLeft ? "│   " : "    "), false);
+    }
+
+}
+
+
+
+
+
